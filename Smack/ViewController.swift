cbad8fdff5ef3ce0a182aba449570f7a371f3e6a
Patrick Quilty <pquilty79@gmail.com>
2018-07-07 14:18:45 -0500

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Patrick Quilty on 7/7/18.
+//  Copyright © 2018 Patrick Quilty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
