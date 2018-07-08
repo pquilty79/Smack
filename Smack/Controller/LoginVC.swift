f6510cd273b751874d8621d4b56f99e8447064a8
Patrick Quilty <pquilty79@gmail.com>
2018-07-08 15:45:14 -0500

    set up loginVC and UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Patrick Quilty on 7/8/18.
+//  Copyright © 2018 Patrick Quilty. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Patrick Quilty on 7/8/18.
+//  Copyright © 2018 Patrick Quilty. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
