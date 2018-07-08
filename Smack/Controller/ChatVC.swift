34edeea61b1ac1d7202660f7f644524f438a3e37
Patrick Quilty <pquilty79@gmail.com>
2018-07-08 13:19:57 -0500

    added reveal functionality

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class ChatVC: UIViewController {
 
+    
+    //Outlets
+    @IBOutlet weak var menuButtun: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        menuButtun.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
+        self.view.addGestureRecognizer((self.revealViewController()?.tapGestureRecognizer())!)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Smack/SupportingFiles/SWRevealViewController.m
+++ Smack/SupportingFiles/SWRevealViewController.m
@@ -479,11 +479,6 @@ static CGFloat scaledValue( CGFloat v1, CGFloat min2, CGFloat max2, CGFloat min1
     return _view.bounds;
 }
 
-- (void)pauseInteractiveTransition { 
-    <#code#>
-}
-
-
 @end
 
 
