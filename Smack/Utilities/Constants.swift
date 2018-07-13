220d94e1f9808bc0436c86aaee2aa9c3c52d8d44
Patrick Quilty <pquilty79@gmail.com>
2018-07-13 16:16:09 -0500

    Set up register user web request

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,18 @@
 
 import Foundation
 
-//Segues
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
 
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+// URL Constants
+let BASE_URL = "https://anothersmack.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
